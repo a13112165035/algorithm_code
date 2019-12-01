49db72a266486030dddb923019612b665499096c
tw180021104320 <382065219@qq.com>
2019-12-01 14:47:54 +0800

    Create 180021104320谭威.cpp

--- 180021104320谭威.cpp
+++ 180021104320谭威.cpp
@@ -1,52 +1,116 @@
-#include<iostream>
+//单源最短路径问题 分支 限界法求解
+#include <iostream>
+#include <fstream> 
 using namespace std;
-#define N 7 //Six matrix multiplication N=7 
-void matrixMultiply(int **a,int **b,int **c,int ra,int ca,int rb,int cb){
-	if(ca != rb)
-		cout<<"Matrix cannot be multiplied!";
-	for(int i=0;i<ra;i++){
-		for(int j=0;j<cb;j++){
-			int sum=a[i][0]*b[0][j];
-			for(int k=1;k<ca;k++)
-				sum+=a[i][k]*b[k][j];
-			c[i][j]=sum;
-		}
-	} 
-}
 
-void matriChain(int *p,int n,int m[][N],int s[][N]){
-	for(int i=0;i<=n;i++)
-		m[i][i]=0;
-	for(int r=2;r<=n;r++){
-		for(int i=1;i<=n-r+1;i++){
-			int j=i+r-1;
-			m[i][j]=m[i+1][j]+p[i-1]*p[i]*p[j];
-			s[i][j]=i;
-			for(int k=i+1;k<j;k++){
-				int t=m[i][k]+m[k+1][j]+p[i-1]*p[i]*p[j];
-				if(t<m[i][j]){
-					m[i][j]=t;
-					s[i][j]=k;
-				}
+template<class Type>
+class Graph
+{
+	friend int main();
+    public:
+		void ShortesPaths(int);
+	private:
+        int		n,		   //图G的顶点数
+				*prev;     //前驱顶点数组
+ 	    Type	**c,       //图G的领接矩阵
+				*dist;     //最短距离数组
+}; 
+ 
+template<class Type>
+class MinHeapNode
+{
+   friend Graph<Type>;
+   public:
+	   operator int ()const{return length;}
+   private:
+       int       i;		  //顶点编号
+       Type  length;	  //当前路长
+}; 
+ 
+template<class Type>
+void Graph<Type>::ShortesPaths(int v)//单源最短路径问题的优先队列式分支限界法
+{ 
+	MinHeap<MinHeapNode<Type>> H(1000);
+	MinHeapNode<Type> E;
+ 
+	//定义源为初始扩展节点
+	E.i=v;
+	E.length=0;
+	dist[v]=0;
+ 
+	while (true)//搜索问题的解空间
+	{
+		for (int j = 1; j <= n; j++)
+			if ((c[E.i][j]!=0)&&(E.length+c[E.i][j]<dist[j])) {
+ 
+				 dist[j]=E.length+c[E.i][j];
+				 prev[j]=E.i;
+ 
+				 MinHeapNode<Type> N;
+				 N.i=j;
+				 N.length=dist[j];
+				 H.Insert(N);
 			}
-		}
+		try 
+		{
+			H.DeleteMin(E); 
+		}        
+		catch (OutOfBounds)
+		{
+			break;
+		}  
+
 	}
 }
-
-void Traceback(int i,int j,int s[][N]){
-	if(i == j)
-		return ;
-	Traceback(i,s[i][j],s);
-	Traceback(s[i][j]+1,j,s);
-	cout<<"Multiply A"<<i<<","<<s[i][j];
-	cout<<"and A"<<(s[i][j]+1)<<","<<j<<endl; 
-}
-
-int main(){
-	int p[N]={30,35,15,5,10,20,25};
-	int m[N][N],s[N][N];
-	matriChain(p,N-1,m,s); 
-    Traceback(1,6,s);
+ 
+int main()
+{  
+	int n=11;
+	int prev[12] = {0,0,0,0,0,0,0,0,0,0,0,0};  
+ 
+	int dist[12]={1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000};
+ 
+	cout<<"单源图的邻接矩阵如下："<<endl;
+	int **c = new int*[n+1];
+ 
+	for(int i=1;i<=n;i++)
+	{
+		c[i]=new int[n+1];
+		for(int j=1; j<=n; j++)
+		{
+			fin>>c[i][j];
+			cout<<c[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+ 
+	int v=1;
+    Graph<int> G;
+    G.n=n;
+ 
+    G.c=c;
+    G.dist=dist;
+    G.prev=prev;
+    G.ShortesPaths(v);
+ 
+    cout<<"从S到T的最短路长是："<<dist[11]<<endl;
+    for (int i = 2; i <= n; i++)
+	{
+		cout<<"prev("<<i<<")="<<prev[i]<<"   "<<endl;
+	}
+ 
+    for (int i = 2; i <= n; i++)
+	{
+		cout<<"从1到"<<i<<"的最短路长是："<<dist[i]<<endl;
+	}
+ 
+	for(int i=1;i<=n;i++)
+	{
+		delete []c[i];
+	}
+ 
+	delete []c;
+	c=0;	
 	return 0;
-
 }
+
