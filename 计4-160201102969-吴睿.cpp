0fb4339306dc40fd9d06f7fddd0deacd26d4b7fa
qq995096942 <58391349+qq995096942@users.noreply.github.com>
2019-12-16 20:39:09 +0800

    Revert "Update 计4-160201102969-吴睿.cpp"
    This reverts commit bff9b46e711014b2a8726202be59462c80e1527a.

--- 计4-160201102969-吴睿.cpp
+++ 计4-160201102969-吴睿.cpp
@@ -1,48 +1,31 @@
-#include<iostream>
-#include<cstring> 
-using namespace std;
+//二分搜索
+#include <stdio.h>
 
-const int N=100;
-int p[N],m[N][N];
-
-void MatrixChan(int p[],int n,int m[][N])
+int binarySearch(int a[], const int& x, int n)
 {
-	for(int i=1;i<=n;++i)
-		m[i][i]=0;
-	for(int r=2;r<=n;++r)
-		for(int i=1;i<=n-r+1;++i)
+	int left = 0, right = n - 1;
+	while (left <= right)
+	{
+		int middle = (left + right) / 2;
+		if (x == a[middle])
 		{
-			int j=i+r-1;
-			m[i][j]=m[i+1][j]+p[i-1]*p[i]*p[j];
-			for(int k=i+1;k<j;++k)
-			{
-				int t=m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[j];
-				if(t<m[i][j])
-					m[i][j]=t;
-			} 
+			return middle;
 		}
+		if (x > a[middle])
+		{
+			left = middle + 1;
+		}
+		else
+		{
+			right = middle - 1;
+		}
+	}
+	return -1;
 }
+
 int main()
 {
-	int n,h,l;
-	while(1)
-	{
-		memset(p,0,sizeof p);
-		memset(m,0,sizeof m);
-		cout<<"请输入矩阵数量"<<endl;
-		cin>>n;
-		for(int i=1;i<=n;++i)
-		{
-			cout<<"请分别输入第"<<i<<"个矩阵的行和列"<<endl;
-			cin>>h>>l;
-			if(i==1)p[0]=h;
-			p[i]=l; 
-		} 
-		MatrixChan(p,n,m);
-		cout<<"矩阵最小连乘数为?"<<m[1][n]<<endl;
-		cout<<"继续计算？是：1，否：0"<<endl;
-		cin>>n;
-		if(n==0)break; 
-	}
-	return 0;
-} 
+	int n = 5;
+	int a[6] = { 1,3,5,7,11,14 };
+	return binarySearch(a, n, 6);
+}
