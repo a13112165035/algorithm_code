8d893267b795b82dbddba1c5dea80a0759dc7e49
unknown <1063944318@qq.com>
2019-12-30 10:51:09 +0800

    Update 拉斯维加斯n后算法 170201104863 张浩宏.cpp

--- 拉斯维加斯n后算法 170201104863 张浩宏.cpp
+++ 拉斯维加斯n后算法 170201104863 张浩宏.cpp
@@ -12,33 +12,33 @@ using namespace std;
 	class RandomNumber  
 	{  
 	    private:  
-	        //当前种子  
+	        
 	        unsigned long randSeed;  
 	    public:  
-	        RandomNumber(unsigned long s = 0);//构造函数，默认值0表示由系统自动产生种子  
-	        unsigned short Random(unsigned long n);//产生0：n-1之间的随机整数  
-	        double fRandom(void);//产生[0,1)之间的随机实数  
+	        RandomNumber(unsigned long s = 0);
+	        unsigned short Random(unsigned long n);
+	        double fRandom(void);
 	};  
 	  
-	RandomNumber::RandomNumber(unsigned long s)//产生种子  
+	RandomNumber::RandomNumber(unsigned long s)
 	{  
 	    if(s == 0)  
 	    {  
-	        randSeed = time(0);//用系统时间产生种子  
+	        randSeed = time(0);
 	    }  
 	    else  
 	    {  
-	        randSeed = s;//由用户提供种子  
+	        randSeed = s;
 	    }  
 	}  
 	  
-	unsigned short RandomNumber::Random(unsigned long n)//产生0：n-1之间的随机整数  
+	unsigned short RandomNumber::Random(unsigned long n)
 	{  
-	    randSeed = multiplier * randSeed + adder;//线性同余式  
+	    randSeed = multiplier * randSeed + adder; 
 	    return (unsigned short)((randSeed>>16)%n);  
 	}  
 	  
-	double RandomNumber::fRandom(void)//产生[0,1)之间的随机实数  
+	double RandomNumber::fRandom(void)
 	{  
 	    return Random(maxshort)/double(maxshort);  
 	}  	
